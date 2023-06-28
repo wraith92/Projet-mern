5ef12c1540b3fdb829a3b43ef13dbda7e49c0a50
Abderrahman BENSALAH <bensalahabdelrrahman1@gmail.com>
2023-06-28 16:20:32 +0200

    branch abderrahman

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require('express');
+let cors = require('cors')
+
+const Users = require('./routes/api/Users')
+
+const app = express();
+
+const connectDB = require('./db/conn');
+
+app.use(cors())
+app.use(express.json())
+
+connectDB();
+
+
+// app.use('/api/')
+
+app.listen(3000, () => {
+  console.log(`Server running on port 3000 connecting to MongoDB`);
+});
+
+
+console.log('hello world')
+
